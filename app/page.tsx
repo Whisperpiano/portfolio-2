0cd6f79e1c0c7725a104e155d4f53af766ee2726
Whisperpiano <jesusalberola90@gmail.com>
2025-05-28 12:31:14 +0200

    feat: implement portfolio pages with project routing and basic UI components

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,6 @@
 import { Button } from '@/components/ui/button';
+import { PROJECTS } from '@/lib/data/projects';
+import Link from 'next/link';
 
 export default function Home() {
   return (
@@ -6,6 +8,12 @@ export default function Home() {
       <h1 className="my-5 text-3xl font-bold text-red-500">Home</h1>
       <p className="text-xl">Welcome to my portfolio</p>
       <Button>Button</Button>
+
+      {PROJECTS.map((project) => (
+        <Link key={project.slug} href={`/works/${project.slug}`}>
+          {project.title}
+        </Link>
+      ))}
     </section>
   );
 }
---
+++ app/works/[slug]/page.tsx
@@ -0,0 +1,22 @@
+import { PROJECTS } from '@/lib/data/projects';
+import { notFound } from 'next/navigation';
+
+export default async function Work({ params }: { params: { slug: string } }) {
+  const { slug } = await params;
+  const project = PROJECTS.find((project) => project.slug === slug);
+
+  if (!project) {
+    return notFound();
+  }
+
+  return (
+    <section>
+      <h1>Work</h1>
+      <p>Here you can find my work</p>
+      <p>{project?.title}</p>
+      <p>{project?.description}</p>
+      <img src={project?.image} alt={project?.title} />
+      <a href={project?.link}>Link</a>
+    </section>
+  );
+}
