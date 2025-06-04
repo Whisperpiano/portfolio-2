c58e7ef4777c5e1f8415537aea6ec3765645fa67
Whisperpiano <jesusalberola90@gmail.com>
2025-06-04 08:36:22 +0200

    fix social media links

--- lib/data/socials.ts
+++ lib/data/socials.ts
@@ -10,38 +10,38 @@ import {
 export const SOCIALS = [
   {
     name: 'LinkedIn',
-    href: 'https://linkedin.com/in/jesus',
+    href: 'https://www.linkedin.com/in/jes%C3%BAs-alberola-herrero-896b61189/',
     class: 'hover:bg-[#0077B575]',
     icon: FaLinkedin,
   },
   {
     name: 'GitHub',
-    href: 'https://github.com/jesus',
+    href: 'https://github.com/Whisperpiano',
     class: 'hover:bg-[#24292e75]',
     icon: FaGithub,
   },
   {
     name: 'Email',
-    href: 'mailto:jesus@jesus.com',
+    href: 'mailto:jesusalberola90@gmail.com',
     class: 'hover:bg-[#D1483675]',
     icon: FaEnvelope,
   },
 
   {
     name: 'Instagram',
-    href: 'https://instagram.com/jesus',
+    href: 'https://instagram.com/whispers_piano',
     class: 'hover:bg-[#E1306C75]',
     icon: FaInstagram,
   },
   {
     name: 'Facebook',
-    href: 'https://facebook.com/jesus',
+    href: 'https://facebook.com/share/16kJ27WnNN',
     class: 'hover:bg-[#1877F275]',
     icon: FaFacebook,
   },
   {
     name: 'Twitter',
-    href: 'https://twitter.com/jesus',
+    href: 'https://twitter.com/Whisper_piano',
     class: 'hover:bg-[#1DA1F275]',
     icon: FaXTwitter,
   },
